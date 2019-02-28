905c2028c82e7dedba9f91c6df6a3a405c6af9ae
Faiber <bigfaiber.greatfaiber@gmail.com>
2019-02-28 14:38:37 -0500

    P-feature-#19y#21

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,18 @@
 class WelcomeController < ActionController::API
   def index
+    @incidents = Incident.all
   end
 
   def createIncident
+    Incident.create(params)
+  end
+
+  def incidentList
+    @incident_list = Incident.order('created_at DESC')[0...5]
   end
 
   # Implements a service to do a filter
   def Filter
   end
+
 end
