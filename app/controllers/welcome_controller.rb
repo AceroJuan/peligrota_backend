80f4e63ed53de9bd9683db65bbf827860f661380
Faiber <bigfaiber.greatfaiber@gmail.com>
2019-03-01 09:46:55 -0500

    P-feature-#19y#21_JSON INCLUDED

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,26 @@
 class WelcomeController < ActionController::API
   def index
     @incidents = Incident.all
+    render json: @incidents.to_json
   end
 
   def createIncident
-    Incident.create(params)
+    Incident.create(indicent_params)
+    render status:200, json: {message: 'ok'}.to_json
   end
 
   def incidentList
     @incident_list = Incident.order('created_at DESC')[0...5]
+    render json: @incident_list.to_json
   end
 
   # Implements a service to do a filter
   def Filter
+  end
+
+  private
+  def incident_params
+    params.require(:incidents).permit(:longitude, :latitude, :danger_level, :description, :datetime, :creator_name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "incidents", to: "welcome#index"
+  get "latest_incidents", to: "welcome#incidentList"
 end
